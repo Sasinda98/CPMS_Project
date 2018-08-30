cf45fd0fe33951c7bdda50b515cdbafdfb746b1e
Chandula Harasgama <chandula567@gmail.com>
2018-08-30 19:32:50 +0530

    updated icon for masonry category

--- app/src/main/java/com/construction/app/cpms/inventoryManagement/inventory_items_list.java
+++ app/src/main/java/com/construction/app/cpms/inventoryManagement/inventory_items_list.java
@@ -27,55 +27,55 @@ public class inventory_items_list extends AppCompatActivity {
     private ArrayList getData(){
          ArrayList<inventory_item_Bean> itemBeanList = new ArrayList<>();
 
-        inventory_item_Bean inventoryItemBean = new inventory_item_Bean("Cement", "100","Masonry" , "Bags", R.drawable.hammer);
+        inventory_item_Bean inventoryItemBean = new inventory_item_Bean("Cement", "100","Masonry" , "Bags", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("Bricks", "2500","Masonry" , "Nos.", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("Bricks", "2500","Masonry" , "Nos.", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("Cinder Blocks", "2500","Masonry" , "Nos.", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("Cinder Blocks", "2500","Masonry" , "Nos.", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("Sand", "50","Masonry" , "Cubes.", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("Sand", "50","Masonry" , "Cubes.", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("Rubble", "250","Masonry" , "tonnes.", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("Rubble", "250","Masonry" , "tonnes.", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("Metal", "40", "Masonry", "tonnes", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("Metal", "40", "Masonry", "tonnes", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("Lime", "250","Masonry" , "Cubes", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("Lime", "250","Masonry" , "Cubes", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("Plywood", "2500","Masonry" , "Nos.", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("Plywood", "2500","Masonry" , "Nos.", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("Shuttering", "2500","Masonry" , "Nos.", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("Shuttering", "2500","Masonry" , "Nos.", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("2X2 Timber", "2500","Masonry" , "Nos.", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("2X2 Timber", "2500","Masonry" , "Nos.", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("4X2 Timber", "2500","Masonry" , "Nos.", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("4X2 Timber", "2500","Masonry" , "Nos.", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("10mm Tor Steel", "2500","Masonry" , "Meters", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("10mm Tor Steel", "2500","Masonry" , "Meters", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("12mm Tor Steel", "2500","Masonry" , "Meters", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("12mm Tor Steel", "2500","Masonry" , "Meters", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("16mm Tor Steel", "2500","Masonry" , "Meters", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("16mm Tor Steel", "2500","Masonry" , "Meters", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("08mm Tor Steel", "2500","Masonry" , "Meters", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("08mm Tor Steel", "2500","Masonry" , "Meters", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("1.5' Wire Nails", "2500","Masonry" , "Nos.", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("1.5' Wire Nails", "2500","Masonry" , "Nos.", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
 
-        inventoryItemBean = new inventory_item_Bean("3' Wire Nails", "2500","Masonry" , "Nos.", R.drawable.hammer);
+        inventoryItemBean = new inventory_item_Bean("3' Wire Nails", "2500","Masonry" , "Nos.", R.drawable.brickwall);
         itemBeanList.add(inventoryItemBean);
         return itemBeanList;
     }
