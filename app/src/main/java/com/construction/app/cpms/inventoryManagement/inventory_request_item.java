ea0c88548d8b36987588331336054b4016f9d0e9
Chandula Harasgama <chandula567@gmail.com>
2018-09-09 14:27:44 +0530

    Requesting an Item now works
    Still Have to add validation checks

--- app/src/main/java/com/construction/app/cpms/inventoryManagement/inventory_request_item.java
+++ app/src/main/java/com/construction/app/cpms/inventoryManagement/inventory_request_item.java
@@ -1,14 +1,28 @@
 package com.construction.app.cpms.inventoryManagement;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.construction.app.cpms.R;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class inventory_request_item extends AppCompatActivity {
 
@@ -17,6 +31,13 @@ public class inventory_request_item extends AppCompatActivity {
     private String itemName;
     private int itemID;
     private TextView reqItemTextView;
+    private TextInputEditText reqMessage = null;
+    private TextInputEditText reqQty = null;
+    private Button confirmBtn;
+
+    private RequestQueue requestQueue;
+    private String insertUrl = "https://projectcpms99.000webhostapp.com/scripts/chandula/insertInventoryRequest.php";
+    private StringRequest stringRequest;
 
 
     @Override
@@ -30,8 +51,17 @@ public class inventory_request_item extends AppCompatActivity {
         itemName = intent.getStringExtra("itemName");
         itemID = intent.getIntExtra("itemID", 0);
 
+
+        //Initialising all the stuff on the xml
         reqItemTextView = (TextView) findViewById(R.id.request_item_name);
         reqItemTextView.setText(itemName);
+        confirmBtn = (Button) findViewById(R.id.request_item_confirm_btn);
+        reqMessage = findViewById(R.id.inventory_request_message);
+        reqQty = findViewById(R.id.inventory_request_qty);
+
+
+        addListenerOnConfirm();
+
 
         //Getting current time to pass to script
         currentTime = now();
@@ -48,4 +78,56 @@ public class inventory_request_item extends AppCompatActivity {
         SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT_NOW);
         return sdf.format(cal.getTime());
     }
+
+
+    private void addListenerOnConfirm() {
+
+
+        requestQueue = Volley.newRequestQueue(this.getApplicationContext());
+
+
+        //Linking the views on xml file to java file
+
+
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                StringRequest request = new StringRequest(Request.Method.POST, insertUrl, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        HashMap<String,String> params = new HashMap<>();
+                        params.put("iID", Integer.toString(itemID));
+                        params.put("rQty", reqQty.getText().toString());
+                        params.put("rMsg", reqMessage.getText().toString());
+                        params.put("rDate", currentTime);
+                        return params;
+                    }
+                };
+
+                requestQueue.add(request);
+
+                CharSequence msg = "RequestSent";
+                Toast.makeText(inventory_request_item.this, msg, Toast.LENGTH_LONG).show();
+
+
+
+
+            }
+        });
+    }
+
+
 }
