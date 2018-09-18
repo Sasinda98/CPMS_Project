d1c493c27787cc19d5f1bf2b4d6737d0471b1d4a
Sasinda <sasindarupasinghe@icloud.com>
2018-09-18 23:13:00 +0530

    #1 minor additions -GSR

--- app/src/main/java/com/construction/app/cpms/miscellaneous/ProfileActivity.java
+++ app/src/main/java/com/construction/app/cpms/miscellaneous/ProfileActivity.java
@@ -8,15 +8,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.construction.app.cpms.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -24,8 +31,9 @@ public class ProfileActivity extends AppCompatActivity {
 
     private FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-
     private final static String TAG = "ProfileActivity";
+
+    private CircleImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +41,21 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         MaterialButton materialButton = findViewById(R.id.buttonBadluck);
+        imageView = findViewById(R.id.actprofile_profile_pic);
+
+        displayProfilePicInImageView(imageView, firebaseUser, firebaseDatabase);
 
         materialButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // setProfPic("https://firebasestorage.googleapis.com/v0/b/cpms-4780c.appspot.com/o/user%2FJw405DV177dkOg2nBWAjsAERs8j1%2FprofilePic%2Funnamed.jpg?alt=media&token=3597b2d3-6a6c-4fb2-8449-0dbe8ca095bb");
+                // setProfPic("https://firebasestorage.googleapis.com/v0/b/cpms-4780c.appspot.com/o/user%2FJw405DV177dkOg2nBWAjsAERs8j1%2FprofilePic%2Funnamed.jpg?alt=media&token=3597b2d3-6a6c-4fb2-8449-0dbe8ca095bb");
                 setProfPicFireDB("https://firebasestorage.googleapis.com/v0/b/cpms-4780c.appspot.com/o/user%2FJw405DV177dkOg2nBWAjsAERs8j1%2FprofilePic%2Funnamed.jpg?alt=media&token=3597b2d3-6a6c-4fb2-8449-0dbe8ca095bb");
             }
         });
-}
+    }
 
-    public void setProfPic(String url){
+    /*Do not use*/
+    public void setProfPic(String url) {
         UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
                 .setPhotoUri(Uri.parse(url))
                 .build();
@@ -60,10 +72,39 @@ public class ProfileActivity extends AppCompatActivity {
 
     }
 
-    public void setProfPicFireDB(String url){
-       DatabaseReference reference = firebaseDatabase.getReference().getRoot();
-       reference.child("users").child(firebaseUser.getUid()).child("photoUrl").setValue(url);
+    /* sets the photoUrl of profile pic in firebase databsae, root/users/$UID/photoUrl */
+    public void setProfPicFireDB(String url) {
+        DatabaseReference reference = firebaseDatabase.getReference().getRoot();
+        reference.child("users").child(firebaseUser.getUid()).child("photoUrl").setValue(url);
     }
 
+    public void displayProfilePicInImageView(final CircleImageView circleImageView, FirebaseUser firebaseUser, FirebaseDatabase firebaseDatabase) {
 
+        if (firebaseUser != null) {
+
+            circleImageView.setImageResource(R.drawable.ic_prof_pic_dark);       //default profile pic, until one load from firebase.
+
+            DatabaseReference reference = firebaseDatabase.getReference().getRoot();
+
+            reference.child("users").child(firebaseUser.getUid()).child("photoUrl").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String url = (String) dataSnapshot.getValue();
+
+                    if (url != null || url != "") {
+                        Glide.with(getApplicationContext()).load(url)
+                                .asBitmap().into(circleImageView);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+
+
+    }
 }
